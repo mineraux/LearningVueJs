61da96f449abfda89fd2c97c1e8cc2901d64c490
Minervini Robin <robin.minervini@gmail.com>
2018-07-05 23:19:11 +0200

    Learning vue components and naming convention using es6

---
+++ part2-vue_cli/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <rm-header></rm-header>
+        <hr>
+        <div class="row">
+            <rm-servers></rm-servers>
+            <rm-serverDetails></rm-serverDetails>
+        </div>
+        <hr>
+        <rm-footer></rm-footer>
+    </div>
+</template>
+
+<script>
+    import rmHeader from './components/Shared/Header.vue'
+    import rmFooter from './components/Shared/Footer.vue'
+    import rmServers from './components/Server/Servers.vue'
+    import rmServerDetails from './components/Server/ServerDetails.vue'
+
+    export default {
+        components : {
+            rmHeader,
+            rmFooter,
+            rmServers,
+            rmServerDetails
+        }
+    }
+</script>
+
+<style>
+
+</style>
