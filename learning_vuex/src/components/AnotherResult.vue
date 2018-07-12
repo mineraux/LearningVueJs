a574e076724be086248c217f7d23bc88a0016004
Minervini Robin <robin.minervini@gmail.com>
2018-07-12 11:33:44 +0200

    Mapping getters to properties

--- learning_vuex/src/components/AnotherResult.vue
+++ learning_vuex/src/components/AnotherResult.vue
@@ -1,19 +1,18 @@
 <template>
     <div>
-        <p>Counter is: {{ counter }}</p> 
-        <p>Number of clicks: {{ clicks }}</p>       
+        <p>Counter is: {{ doubleCounter }}</p> 
+        <p>Number of clicks: {{ stringCounter }}</p>       
     </div>
 </template>
 
 <script>
-export default {
+import { mapGetters } from 'vuex'
+export default {    
     computed: {
-        counter() {
-            return this.$store.getters.doubleCounter
-        },
-        clicks() {
-            return this.$store.getters.stringCounter
-        }
+        ...mapGetters([
+        'doubleCounter',
+        'stringCounter'
+    ])
     }
 }
 </script>
