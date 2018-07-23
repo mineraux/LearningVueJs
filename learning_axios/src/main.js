4c1c218039d9f947dbc458aabe7a60ceca62981c
Minervini Robin <robin.minervini@gmail.com>
2018-07-23 16:16:39 +0200

    Learning axios with vue

---
+++ learning_axios/src/main.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import App from './App.vue'
+import axios from 'axios'
+
+import router from './router'
+import store from './store'
+
+axios.defaults.baseURL = 'https://vuejs-axios-5970d.firebaseio.com'
+axios.defaults.headers.common['Authorization'] = 'Hello'
+axios.defaults.headers.get['Accepts'] = 'application.json'
+
+const reqInterceptor = axios.interceptors.request.use(config => {
+  console.log('Request interceptor', config);
+  return config
+})
+
+const resInterceptor = axios.interceptors.response.use(res => {
+  console.log('Response interceptor', res)
+  return res
+})
+
+axios.interceptors.request.eject(reqInterceptor)
+axios.interceptors.response.eject(resInterceptor)
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
